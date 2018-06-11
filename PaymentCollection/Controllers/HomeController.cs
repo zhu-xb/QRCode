1566b4953a7c29782fa5e4cbf3af7f948a2fa9fc
zhu-xb <31944847+zhu-xb@users.noreply.github.com>
2018-06-11 11:07:21 +0800

    添加项目文件。

---
+++ PaymentCollection/Controllers/HomeController.cs
@@ -0,0 +1,91 @@
+﻿using Gma.QrCodeNet;
+using Gma.QrCodeNet.Encoding;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+using Gma.QrCodeNet.Encoding.Windows.WPF;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QRCode.Controllers
+{
+    public class HomeController : Controller
+    {
+        static Random r = new Random();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult BuildQRCode(PXInfo info)
+        {
+            ResponseResult ret = new ResponseResult();
+            try
+            {
+                QrEncoder qrEncoder = new QrEncoder(ErrorCorrectionLevel.H);
+                QrCode qrCode = qrEncoder.Encode(info.ToString());
+                ret.success = qrEncoder.TryEncode(info.ToString(), out qrCode);
+                if (ret.success)
+                {
+                    string filename = DateTime.Now.ToString("yyyyMMddHHmmss") + "_" + r.Next(0, 10000).ToString("D5") + ".png";
+                    GraphicsRenderer render = new GraphicsRenderer(new FixedModuleSize(5, QuietZoneModules.Two), Brushes.Black, Brushes.White);
+                    using (FileStream stream = new FileStream(Server.MapPath("~/qrimages/") + filename, FileMode.Create))
+                    {
+                        render.WriteToStream(qrCode.Matrix, ImageFormat.Png, stream);
+                    }
+
+                    ret.imagename = filename;
+                }
+                else
+                {
+                    ret.errorMsg = "二维码生成失败";
+                }
+            }
+            catch (Exception ex)
+            {
+                ret.success = false;
+                ret.errorMsg = ex.Message;
+            }
+            return Json(ret);
+        }
+    }
+
+    public class ResponseResult
+    {
+        public bool success { get; set; }
+        public string errorMsg { get; set; }
+        public string imagename { get; set; }
+    }
+
+    public class PXInfo
+    {
+        public string name1 { get; set; }
+        public string name2 { get; set; }
+        public string name3 { get; set; }
+        public string name4 { get; set; }
+        public string name5 { get; set; }
+        public string name6 { get; set; }
+        public string name7 { get; set; }
+        public string name8 { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0},{1},{2},{3},{4},{5},{6},{7}"
+                , name1 ?? "", name2 ?? "", name3 ?? ""
+                , (name4 == null ? DateTime.Now.ToString("yyyyMMdd") : Convert.ToDateTime(name4).ToString("yyyyMMdd"))
+                , (name5 == null ? DateTime.Now.ToString("yyyyMMdd") : Convert.ToDateTime(name5).ToString("yyyyMMdd"))
+                , name6 ?? "", name7 ?? "", name8 ?? "");
+        }
+    }
+}
---
+++ PaymentCollection/Scripts/Common.js
@@ -0,0 +1,31 @@
+﻿//日期格式化new Date().format('yyyy-MM-dd hh:mm:ss')
+Date.prototype.format = function (format) {
+    var o = {
+        "M+": this.getMonth() + 1, //month
+        "d+": this.getDate(), //day
+        "h+": this.getHours(), //hour
+        "m+": this.getMinutes(), //minute
+        "s+": this.getSeconds(), //second
+        "q+": Math.floor((this.getMonth() + 3) / 3), //quarter
+        "S": this.getMilliseconds() //millisecond
+    }
+    if (/(y+)/.test(format)) format = format.replace(RegExp.$1,
+    (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o) if (new RegExp("(" + k + ")").test(format))
+        format = format.replace(RegExp.$1,
+        RegExp.$1.length == 1 ? o[k] :
+        ("00" + o[k]).substr(("" + o[k]).length));
+    return format;
+}
+//金额增加千分号
+function formatPrice(val, row) {
+    var parts = val.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return "<span >￥&nbsp;" + parts.join(".") + "&nbsp;</span>";
+
+}
+
+//将/Date(13925635263)/格式化
+function formatDate(val, row) {
+    return new Date(parseInt(val.replace(/\D/igm, ""))).format('yyyy-MM-dd hh:mm');
+}
